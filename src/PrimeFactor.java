3012c5120965c54f26237092935fe00c9dfd16ff
Doolan <doolansr@rose-hulman.edu>
2015-03-24 14:53:39 -0400

    Created files

---
+++ src/PrimeFactor.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author Steven.
+ *         Created Mar 24, 2015.
+ */
+public class PrimeFactor {
+
+	/**
+	 * TODO Put here a description of what this method does.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	}
+
+}
---
+++ src/PrimeFactorTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author Doolan.
+ *         Created Mar 24, 2015.
+ */
+public class PrimeFactorTest {
+
+	@Test
+	public final void test() {
+		fail("Not yet implemented");
+	}
+
+}
